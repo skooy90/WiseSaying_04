6889b7f9fd9eb0ed94735221d5a6b631e62ce557
unknown <skooy90@gmail.com>
2025-04-16 20:22:39 +0900

    설정 가져오기

---
+++ src/main/java/KoreaIT/Main.java
@@ -0,0 +1,59 @@
+package KoreaIT;
+
+import KoreaIT.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Article> articleList = new ArrayList<>();
+
+    System.out.println("== 프로그램 시작 ==");
+
+    int lastId = 0;
+
+    Scanner sc = new Scanner(System.in);
+
+    while(true){
+      System.out.print("명령어) ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("article list")){
+        System.out.println("번호  /  제목  / 내용");
+        for(int i = articleList.size()-1 ; i >= 0;i--){
+          System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(),  articleList.get(i).getBody());
+        }
+      }
+      else if (cmd.equals("article write")){
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+        System.out.print("내용 : ");
+        String body = sc.nextLine();
+
+        lastId++;
+        int id = lastId;
+
+        Article addArticle = new Article();
+        addArticle.setId(id);
+        addArticle.setTitle(title);
+        addArticle.setBody(body);
+
+        articleList.add(addArticle);
+
+        System.out.printf("%d번 글이 생성되었습니다.\n", id);
+      }
+
+      else if(cmd.equals("exit")){
+        System.out.println("== 프로그램 종료 ==");
+        break;
+      }
+      else{
+        System.out.println("사용할 수 없는 명령어입니다.");
+      }
+
+    }
+
+  }
+}
