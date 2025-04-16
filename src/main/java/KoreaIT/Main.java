550ffb2481d38c81cefedb995044f1b2d93d0b93
unknown <skooy90@gmail.com>
2025-04-16 21:32:44 +0900

    detail에서 날짜시간 정보 나오도록 구현

--- src/main/java/KoreaIT/Main.java
+++ src/main/java/KoreaIT/Main.java
@@ -1,13 +1,16 @@
 package KoreaIT;
 
-import KoreaIT.Article;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+
+//        Test.test();
+
     List<Article> articleList = new ArrayList<>();
 
     System.out.println("== 프로그램 시작 ==");
@@ -16,21 +19,65 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
-    while(true){
+    while (true) {
       System.out.print("명령어) ");
-      String cmd = sc.nextLine();
+      String cmd = sc.nextLine().trim();
 
-      if (cmd.equals("article list")){
-        System.out.println("번호  /  제목  / 내용");
-        for(int i = articleList.size()-1 ; i >= 0;i--){
-          System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(),  articleList.get(i).getBody());
+      if (cmd.startsWith("article detail")) {
+        // parsing start
+        String[] cmdBits = cmd.split(" ");
+
+        if (cmdBits.length > 3) {
+          System.out.println("명령어를 제대로 입력해주세요.");
         }
-      }
-      else if (cmd.equals("article write")){
+        int detailId = -1;
+        try {
+          detailId = Integer.parseInt(cmdBits[2]);
+        } catch (NumberFormatException e) {
+          System.out.println("정수를 제대로 입력해주세요.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+          System.out.println("정수를 추가해서 입력해주세요.");
+        }
+        // parsing end
+
+        boolean flag = true;
+        if (!articleList.isEmpty()) {
+          for (Article article : articleList) {
+            if (article.getId() == detailId) {
+              flag = false;
+              System.out.println("번호 : " + article.getId());
+              System.out.println("날짜 : " + article.getRegDate());
+              System.out.println("제목 : " + article.getTitle());
+              System.out.println("내용 : " + article.getBody());
+            }
+          }
+          if (flag == true) {
+            System.out.printf("%d번 게시글은 없습니다\n", detailId);
+          }
+        } else {
+          System.out.println("게시글이 아예 없습니다.");
+        }
+
+
+      } else if (cmd.equals("article list")) {
+
+        System.out.println("번호  /  제목  / 내용");
+        for (int i = articleList.size() - 1; i >= 0; i--) {
+          System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(), articleList.get(i).getBody());
+        }
+      } else if (cmd.equals("article write")) {
+
         System.out.print("제목 : ");
         String title = sc.nextLine();
         System.out.print("내용 : ");
         String body = sc.nextLine();
+
+//                현재 일시 불어와서 형식 변경
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String formatedNow = now.format(formatter);
+        String regDate = formatedNow;
+
 
         lastId++;
         int id = lastId;
@@ -39,17 +86,18 @@ public class Main {
         addArticle.setId(id);
         addArticle.setTitle(title);
         addArticle.setBody(body);
+        addArticle.setRegDate(regDate);
 
         articleList.add(addArticle);
 
         System.out.printf("%d번 글이 생성되었습니다.\n", id);
-      }
+      } else if (cmd.equals("exit")) {
 
-      else if(cmd.equals("exit")){
+
         System.out.println("== 프로그램 종료 ==");
         break;
-      }
-      else{
+      } else {
+
         System.out.println("사용할 수 없는 명령어입니다.");
       }
 
